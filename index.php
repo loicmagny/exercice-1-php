3ed957ef2829861b979d45b7826e5dce3910fc51
loicmagny <loicmagn60@gmail.com>
2017-09-29 13:37:17 +0200

    Fin exercice 1

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+  <body>
+  <?php
+  $nombre1 = 2 + 4;
+  $nombre2 = 2 * 4;
+  print $nombre1;
+  print $nombre2;
+  ?>
+  </body>
+</html>
